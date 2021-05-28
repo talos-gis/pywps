4b9185974d7b8926429f37bfbc3b21e98abe9779
Idan Miara <idan@miara.com>
2021-05-28 09:18:33 +0300

    v4.2.11.13

--- pywps/__init__.py
+++ pywps/__init__.py
@@ -9,7 +9,7 @@ import os
 
 from lxml.builder import ElementMaker
 
-__version__ = '4.2.11.12'
+__version__ = '4.2.11.13'
 
 LOGGER = logging.getLogger('PYWPS')
 LOGGER.debug('setting core variables')
